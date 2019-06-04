85f84038ccdc6c8507234ec65ff23b937b3fc934
Administrator <1964375786@qq.com>
2019-06-04 18:20:22 +0800

    增加了properties值的写法

--- src/main/java/com/xyb/springboot02/bean/Dog.java
+++ src/main/java/com/xyb/springboot02/bean/Dog.java
@@ -5,14 +5,6 @@ public class Dog {
     private String name;
     private Integer age;
 
-    @Override
-    public String toString() {
-        return "Dog{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
     public String getName() {
         return name;
     }
@@ -25,7 +17,16 @@ public class Dog {
         return age;
     }
 
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
     public void setAge(Integer age) {
         this.age = age;
     }
+
 }
